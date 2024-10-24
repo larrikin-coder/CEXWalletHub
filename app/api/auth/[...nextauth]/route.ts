edb7b1a39ea26ebe0a2b32653bb2073d451a21ce
larrikin-coder <shauryat.22703@gmail.com>
2024-10-25 01:13:57 +0530

    schema created, prisma orm, prisma client installed, postgres connected

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,12 +1,50 @@
 import NextAuth from "next-auth"
 import GoogleProvider from 'next-auth/providers/google'
+import db from "@/db";
+
 const handler = NextAuth({
     providers:[
         GoogleProvider({
         clientId: process.env.GOOGLE_CLIENT_ID ?? "",
         clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
         })
-    ]
+    ],
+    callbacks:{
+        async signIn({user,account,profile,email,credentials}){
+            if (account?.provider === "google"){
+                const email = user.email;
+                if (!email){
+                    return false
+                }
+                const userDb = await db.user.findFirst({
+                    where:{
+                        username: email
+                    }
+                });
+                if (userDb){
+                    return true
+                }
+                await db.user.create({
+                    data:  {
+                        username: email,
+                        provider: "Google",
+                        solWallet:{
+                            create: {
+                                publicKey: "",
+                                privateKey: ""
+                            }
+                        },
+                        inrWallet:{
+                            create: {
+                                balance : 0
+                            }
+                        }
+                    }
+                })
+            }
+            return false
+        }
+    }
 })
 
 export { handler as GET, handler as POST }
